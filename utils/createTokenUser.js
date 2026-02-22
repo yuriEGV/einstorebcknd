9b196e4f4acf4077c20a2bd51841dad265ef2814
Yuri EGV <yuriEGV@users.noreply.github.com>
2026-02-22 11:42:18 -0300

    Fix: Include email in createTokenUser for profile updates

--- utils/createTokenUser.js
+++ utils/createTokenUser.js
@@ -1,5 +1,5 @@
 const createTokenUser = (user) => {
-  return { name: user.name, userId: user._id, role: user.role, dni: user.dni, phone: user.phone };
+  return { name: user.name, userId: user._id, role: user.role, dni: user.dni, phone: user.phone, email: user.email };
 };
 
 module.exports = createTokenUser;
