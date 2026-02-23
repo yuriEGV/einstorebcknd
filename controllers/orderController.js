495f7d6303034b22c658de1901a78ecdae5f6223
Yuri EGV <yuriEGV@users.noreply.github.com>
2026-02-23 08:38:12 -0300

    Fix: Downgrade Node version to 20.x and safer Stripe init to resolve 500 error

--- controllers/orderController.js
+++ controllers/orderController.js
@@ -7,7 +7,7 @@ const CustomError = require('../errors');
 const { checkPermissions } = require('../utils');
 
 const mercadopago = require('mercadopago');
-const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
+const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY || 'sk_test_placeholder');
 const { sendOrderNotification } = require('../utils/twilio');
 
 // Configure Mercado Pago
